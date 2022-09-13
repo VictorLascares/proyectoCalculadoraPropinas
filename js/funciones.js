25a15b93fe725464f1c157a3dc82feeb93ca149f
Victor Lascares <vmlascares@gmail.com>
2022-09-13 15:20:29 -0500

    Mostrando en pantalla los platillos obtenidos en la consulta a la API

--- js/funciones.js
+++ js/funciones.js
@@ -4,6 +4,12 @@ let cliente = {
     pedido: []
 };
 
+const categorias = {
+    1: 'Comidas',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 
 function guardarCliente() {
     const mesa = document.querySelector('#mesa').value;
@@ -35,8 +41,45 @@ function obtenerPlatillos() {
     const url = 'http://localhost:4000/platillos';
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado))
+        .then(resultado => mostrarPlatillos(resultado))
         .catch(error => console.log(error));
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido');
+    platillos.forEach(platillo => {
+        const row = document.createElement('tr');
+
+        const nombre = document.createElement('td');
+        nombre.textContent = platillo.nombre;
+        nombre.classList.add('col-md-4');
+
+        const precio = document.createElement('td');
+        precio.textContent = `$${platillo.precio}.00`;
+        precio.classList.add('col-md-3');
+
+        const categoria = document.createElement('td');
+        categoria.textContent = categorias[platillo.categoria];
+        categoria.classList.add('col-md-3');
+
+
+        const contInput = document.createElement('td');
+        contInput.classList.add('col-md-2');
+        const cantidad = document.createElement('input');
+        cantidad.type = 'number';
+        cantidad.min = 0;
+        cantidad.value = 0;
+        cantidad.id = `platillo-${platillo.id}`;
+
+        contInput.appendChild(cantidad);
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(contInput);
+
+        contenido.appendChild(row);
+    })
 }
 
 function mostrarSecciones() {
